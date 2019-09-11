804275d22d2b734e5f31ebe82a42a00d8d57f69e
Sergey-Hub <sb@finch-melrose.com>
2019-09-11 10:15:03 +0300

    Ex11
    Ex11: Рефакторинг тестов

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,33 +1,21 @@
+
 package lib;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
 import junit.framework.TestCase;
+import lib.ui.WelcomePageObject;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.net.URL;
 
 public class CoreTestCase extends TestCase {
 
-    private static final String PLATFORM_IOS = "ios";
-    private static final String PLATFORM_ANDROID = "android";
-
     protected AppiumDriver driver;
-    private static String AppiumURL = "http://127.0.0.1:4723/wd/hub";
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        DesiredCapabilities capabilities = getCapabilitiesByPlatformEnv();
-        String platform = System.getenv("PLATFORM");
-        if (platform.equals(PLATFORM_ANDROID)) {
-            driver = new AndroidDriver(new URL(AppiumURL), capabilities);
-        } else if (platform.equals(PLATFORM_IOS)) {
-            driver = new IOSDriver(new URL(AppiumURL), capabilities);
-        }
+        driver = Platform.getInstance().getDriver();
         rotateScreenPortrait();
+        skipWelcomePageForIOSApp();
     }
 
     @Override
@@ -48,28 +36,11 @@ public class CoreTestCase extends TestCase {
         driver.runAppInBackground(seconds);
     }
 
-    private DesiredCapabilities getCapabilitiesByPlatformEnv() throws Exception {
-        String platform = System.getenv("PLATFORM");
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        if (platform.equals(PLATFORM_ANDROID)) {
-            capabilities.setCapability("platformName", "Android");
-            capabilities.setCapability("deviceName", "AndroidTestDevice");
-            capabilities.setCapability("platformVersion", "8.0");
-            capabilities.setCapability("automationName", "Appium");
-            capabilities.setCapability("appPackage", "org.wikipedia");
-            capabilities.setCapability("appActivity", ".main.MainActivity");
-            capabilities.setCapability("app", "/Users/user/Desktop/Wikipedia.app");
-        } else if (platform.equals(PLATFORM_IOS)) {
-            capabilities.setCapability("platformName", "iOS");
-            capabilities.setCapability("deviceName", "iPhone SE");
-            capabilities.setCapability("platformVersion", "11.4");
-            capabilities.setCapability("automationName", "Appium");
-            capabilities.setCapability("app", "/Users/user/Desktop/Wikipedia.app");
-        } else {
-            throw new Exception("Cannot run the platform from env variable. Platfrom value: " + platform);
+    private void skipWelcomePageForIOSApp() {
+        if (Platform.getInstance().isIOS()) {
+            WelcomePageObject welcomePageObject = new WelcomePageObject(driver);
+            welcomePageObject.clickSkip();
         }
-        return capabilities;
     }
 
 }
